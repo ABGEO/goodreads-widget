43630f26bf305214232b64c4381a926c5cfb5ce5
Temuri Takalandze <takalandzet@gmail.com>
2022-04-18 16:33:30 +0400

    feat: base64encode images

--- app/views/goodreads.py
+++ app/views/goodreads.py
@@ -1,3 +1,5 @@
+from base64 import b64encode
+
 import requests
 from bs4 import BeautifulSoup
 from flask import Blueprint, Response, request, render_template
@@ -25,6 +27,11 @@ def index():
 
     for link in content.findAll('a'):
         link["href"] = f"{BASE_URL}{link['href']}"
+
+    for img in content.findAll('img'):
+        response = requests.get(img["src"])
+        base64_encoded = b64encode(response.content).decode("ascii")
+        img["src"] = f"data:image/png;base64,{base64_encoded}"
 
     content_raw = ""
     for child in content.children:
